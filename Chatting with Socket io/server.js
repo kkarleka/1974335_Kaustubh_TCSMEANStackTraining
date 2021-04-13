ca5141b36c816662c3ffe281ad508729fd218180
kkarleka <62725295+kkarleka@users.noreply.github.com>
2021-04-13 16:50:37 -0400

    Add files via upload
    New files added

---
+++ Chatting with Socket io/server.js
@@ -0,0 +1,18 @@
+let app = require("express")();
+let http = require("http").Server(app);   // to load the library we have run port number using hhtp module 
+let io = require("socket.io")(http);
+
+app.get("/",(req,res)=> {
+    console.log("Directory path is " , __dirname)
+    res.sendFile(__dirname+"/index.html");
+})
+
+io.on("connection",(socket)=> {
+    console.log("Client connected to application.....");
+    
+    socket.on("chat",(msg)=> {
+        console.log(msg);
+    })
+})
+
+http.listen(9090,()=>console.log('server running on port number 9090'));
